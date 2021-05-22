de9c329c5fb3ccb550d9becdca793310953607e2
rolandjlevy <rolandjlevy@gmail.com>
2021-05-22 22:58:09 +0100

    Added reducer and store

--- src/index.js
+++ src/index.js
@@ -1,51 +1,12 @@
-// functions are first class citizens
+import store from './store';
 
-function greet(fn) {
-  console.log(fn());
-}
+store.dispatch({
+  type: 'BUG_ADDED',
+  payload: {
+    description: 'bug_1'
+  }
+});
 
-function sayHello() {
-  return function() {
-    return "Hello world";
-  };
-}
+const state = store.getState();
 
-// functional composing and piping using lodash
-
-import { compose, pipe } from 'lodash/fp';
-
-let input = "    JavaScript      ";
-
-let output = "div" + input.trim() + "</div>";
-
-const trim = str => str.trim();
-const wrapInDiv = str => `<div>${str}</div>`;
-const toLowerCase = str => str.toLowerCase();
-
-const result = wrapInDiv(toLowerCase(trim(input))); // difficult to read
-
-const transformCompose = compose(wrapInDiv, toLowerCase, trim);
-const resultComposed = transformCompose(input);
-
-const transformPipe = pipe(trim, toLowerCase, wrapInDiv);
-const resultPiped = transformPipe(input);
-console.log({resultPiped});
-
-// use currying
-
-const wrap = type => str => `<${type}>${str}</${type}>`;
-
-const transformPipeCurry = pipe(trim, toLowerCase, wrap('span'));
-const resultPipedCurry = transformPipeCurry(input);
-console.log({resultPipedCurry});
-
-// Updating object properties
-
-const person = { name: 'John' };
-const updated = Object.assign({}, person, { name: 'Bob'});
-console.log({updated});
-const updateSpread = { ...person, name: 'Bob' };
-console.log({updateSpread});
-
-// Immutable.js and Immer library for immutable data structures
-
+console.log({state});
