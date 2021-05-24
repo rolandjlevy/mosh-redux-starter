ce7859e05f52e47c02b1baa66ffd85125a28798e
rolandjlevy <rolandjlevy@gmail.com>
2021-05-24 11:22:52 +0100

    Added actions

---
+++ src/actions.js
@@ -0,0 +1,11 @@
+import * as actions from './actionTypes';
+
+export const bugAdded = description => ({
+  type: actions.BUG_ADDED,
+  payload: { description }
+});
+
+export const bugRemoved = id => ({
+  type: actions.BUG_REMOVED,
+  payload: { id }
+});
--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import store from './store';
+import * as actions from './actionTypes';
+import { bugAdded, bugRemoved } from './actions'
 
-store.dispatch({
-  type: 'BUG_ADDED',
-  payload: {
-    description: 'bug_1'
-  }
+// This gets called every time the state of the store changes
+// UI components subscribe to the store so they get notofied when the store's state changes
+const unsubscribe = store.subscribe(() => {
+  console.log('store changed', store.getState());
 });
+
+// When we dispatch an action our store calls a reducer which returns the updated state
+// Then all the subscribers get notified
+store.dispatch(bugAdded('This is bug 1'));
+
+unsubscribe();
+
+store.dispatch(bugRemoved(1));
 
 const state = store.getState();
 
